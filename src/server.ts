404c155ca2a295c5715715c3b2e64efe1eaa6110
JoaoGSDC <joaogsdc@gmail.com>
2020-12-05 16:34:20 -0300

    Project #4

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,17 @@ import routes from './routes';
 
 export const app = express();
 
-app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Controll-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type, Authorization');
+
+    app.use(cors());
+    next();
+})
+
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
